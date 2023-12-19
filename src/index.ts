999728aa1552582b04be5a487f4d5054a0c0115f
Pan <pan.1245.dev@gmail.com>
2023-12-19 20:08:33 +0700

    Initialize project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const TOKEN = process.env.TOKEN as string;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+  if (client.user !== null) {
+    console.log(`Logged in as ${client.user.tag}!`);
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(TOKEN);
