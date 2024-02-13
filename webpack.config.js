91bf1cb6df1ecace6d4a743dbd403ebd99874ec3
Pan <pan.1245.dev@gmail.com>
2024-02-13 17:15:35 +0700

    perf: add webpack and nodemon

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import path from "path";
+
+export default {
+  entry: "./src/bot.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
