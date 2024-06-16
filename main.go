fe649224f046ab23477e973ea8c67eea58b9e8ea
Pan <siwach.toprasert@gmail.com>
2024-06-16 23:09:59 +0700

    feat: add simple handlers

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
+	"momo/config"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -16,13 +19,57 @@ func main() {
 		log.Fatal(err)
 	}
 
+	cfg := config.NewConfig()
+
 	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
 
-		if m.Content != "Hello" {
+		args := strings.Split(m.Content, " ")
+		if args[0] != cfg.Prefix {
+			return
+		}
+
+		if args[1] != "Hello" {
 			s.ChannelMessageSend(m.ChannelID, "World!")
+		}
+
+		if args[1] == "Proverbs" {
+			proverbs := []string{
+				"Don't communicate by sharing memory, share memory by communicating.",
+				"Concurrency is not parallelism.",
+				"Channels orchestrate; mutexes serialize.",
+				"The bigger the interface, the weaker the abstraction.",
+				"Make the zero value useful.",
+				"interface{} says nothing.",
+				"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+				"A little copying is better than a little dependency.",
+				"Syscall must always be guarded with build tags.",
+				"Cgo must always be guarded with build tags.",
+				"Cgo is not Go.",
+				"With the unsafe package there are no guarantees.",
+				"Clear is better than clever.",
+				"Reflection is never clear.",
+				"Errors are values.",
+				"Don't just check errors, handle them gracefully.",
+				"Design the architecture, name the components, document the details.",
+				"Documentation is for users.",
+				"Don't panic.",
+			}
+
+			selection := rand.Intn(len(proverbs))
+
+			author := discordgo.MessageEmbedAuthor{
+				Name: "Rob Pike",
+				URL:  "https://go-proverbs.github.io/",
+			}
+			embed := discordgo.MessageEmbed{
+				Title:  proverbs[selection],
+				Author: &author,
+			}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
 		}
 	})
 
