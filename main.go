76e482236beee7323e71ab5bbb34cd0c835547a5
Pan <siwach.toprasert@gmail.com>
2024-07-31 09:33:35 +0700

    feat: add subcommands

--- main.go
+++ main.go
@@ -30,50 +30,56 @@ func main() {
 			return
 		}
 
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "world")
+		}
+
 		args := strings.Split(m.Content, " ")
-		if args[0] != cfg.Prefix {
+
+		if !strings.HasPrefix(m.Content, cfg.Prefix) {
 			return
-		}
+		} else {
+			if args[1] == "hello" {
+				s.ChannelMessageSend(m.ChannelID, "world!")
 
-		if args[1] != "Hello" {
-			s.ChannelMessageSend(m.ChannelID, "World!")
-		}
-
-		if args[1] == "Proverbs" {
-			proverbs := []string{
-				"Don't communicate by sharing memory, share memory by communicating.",
-				"Concurrency is not parallelism.",
-				"Channels orchestrate; mutexes serialize.",
-				"The bigger the interface, the weaker the abstraction.",
-				"Make the zero value useful.",
-				"interface{} says nothing.",
-				"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
-				"A little copying is better than a little dependency.",
-				"Syscall must always be guarded with build tags.",
-				"Cgo must always be guarded with build tags.",
-				"Cgo is not Go.",
-				"With the unsafe package there are no guarantees.",
-				"Clear is better than clever.",
-				"Reflection is never clear.",
-				"Errors are values.",
-				"Don't just check errors, handle them gracefully.",
-				"Design the architecture, name the components, document the details.",
-				"Documentation is for users.",
-				"Don't panic.",
 			}
 
-			selection := rand.Intn(len(proverbs))
+			if args[1] == "proverbs" {
+				proverbs := []string{
+					"Don't communicate by sharing memory, share memory by communicating.",
+					"Concurrency is not parallelism.",
+					"Channels orchestrate; mutexes serialize.",
+					"The bigger the interface, the weaker the abstraction.",
+					"Make the zero value useful.",
+					"interface{} says nothing.",
+					"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+					"A little copying is better than a little dependency.",
+					"Syscall must always be guarded with build tags.",
+					"Cgo must always be guarded with build tags.",
+					"Cgo is not Go.",
+					"With the unsafe package there are no guarantees.",
+					"Clear is better than clever.",
+					"Reflection is never clear.",
+					"Errors are values.",
+					"Don't just check errors, handle them gracefully.",
+					"Design the architecture, name the components, document the details.",
+					"Documentation is for users.",
+					"Don't panic.",
+				}
 
-			author := discordgo.MessageEmbedAuthor{
-				Name: "Rob Pike",
-				URL:  "https://go-proverbs.github.io/",
-			}
-			embed := discordgo.MessageEmbed{
-				Title:  proverbs[selection],
-				Author: &author,
-			}
+				selection := rand.Intn(len(proverbs))
 
-			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+				author := discordgo.MessageEmbedAuthor{
+					Name: "Rob Pike",
+					URL:  "https://go-proverbs.github.io/",
+				}
+				embed := discordgo.MessageEmbed{
+					Title:  proverbs[selection],
+					Author: &author,
+				}
+
+				s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+			}
 		}
 	})
 
